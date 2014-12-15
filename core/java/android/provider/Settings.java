3ac8f56718fe671f0e2cf015d9f0c2fdf6e5977e
jkl5616 <jkl5616@gmail.com>
2014-12-15 16:35:38 +0100

    Add Listanimation Views and Interpolator [1/2]
    PatchSet:
        - optimize more:
        - call provider values only when needed and do not fire them permanently.
          so just do it if scroll state changed.
        - as well go out of the setAnimation method as soon as possible. We
          handle here methods which get called 10000 times during scrolling
          so we need to be as fast as possible.
    
    PS:
    - initialize the animation modes default to off just to be sure
    
    PS:
    - move state change out of OnScrollListener
    - reduce the provider value call to only one call
    
    Change-Id: I5e49d858318420e76101fa6334ff766bd9496c83
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3942,6 +3942,35 @@ public final class Settings {
         public static final String USE_EDGE_SERVICE_FOR_GESTURES = "edge_service_for_gestures";
 
         /**
+         * ListView Animations
+         * 0 == None
+         * 1 == Wave (Left)
+         * 2 == Wave (Right)
+         * 3 == Scale
+         * 4 == Alpha
+         * 5 == Stack (Top)
+         * 6 == Stack (Bottom)
+         * 7 == Translate (Left)
+         * 8 == Translate (Right)
+         * @hide
+         */
+        public static final String LISTVIEW_ANIMATION = "listview_animation";
+
+        /**
+         * ListView Interpolators
+         * 0 == None
+         * 1 == accelerate_interpolator
+         * 2 == decelerate_interpolator
+         * 3 == accelerate_decelerate_interpolator
+         * 4 == anticipate_interpolator
+         * 5 == overshoot_interpolator
+         * 6 == anticipate_overshoot_interpolator
+         * 7 == bounce_interpolator
+         * @hide
+         */
+        public static final String LISTVIEW_INTERPOLATOR = "listview_interpolator";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
