f300bd04a5baa7b4a0d1423e3998dcab0c97f078
bigrushdog <randall.rushing@gmail.com>
2016-10-15 15:05:24 +0200

    [DUI] Initial DUI checkin for N
    -- Take ScreenPinning back to a more AOSP style
       implementation. Credit @Slimroms for the
       ScreenPinningRequest callbacks idea
    
    -- Scrap Car package.
    
    -- cleaner bar size math
    
    -- Handle density configuration changes
    
    AICPfy:
    Bring it to AICP12 (CM14) source by @LorDClockaN
    
    Change-Id: I8f7a0997f60f66928fc0d25b8df71d3e23b611c9

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
@@ -497,7 +497,7 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
     private Runnable mMakeNavigationBarVisibleRunnable = new Runnable() {
         @Override
         public void run() {
-            mPhoneStatusBar.getNavigationBarView().setVisibility(View.VISIBLE);
+            mPhoneStatusBar.getNavigationBarView().getBaseView().setVisibility(View.VISIBLE);
         }
     };
 
@@ -534,7 +534,7 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
                     }
                 } else {
                     mContainer.removeCallbacks(mMakeNavigationBarVisibleRunnable);
-                    mPhoneStatusBar.getNavigationBarView().setVisibility(View.GONE);
+                    mPhoneStatusBar.getNavigationBarView().getBaseView().setVisibility(View.GONE);
                 }
             }
         }
