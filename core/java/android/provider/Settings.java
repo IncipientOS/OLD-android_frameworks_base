0e0c4394223b0f59ea1c2d3fab0839a17189178a
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-12-10 16:32:31 +0100

    base: show battery info on lockscreen when charging [1/2]
    - negociated max. charging current
    - negociated max. charging voltage
    - current battery temperature
    - Formatting improvements by nathanchance and xyyx
    
    Change-Id: Iac2a2194d61612a7af23ca49a4f1c0930d8f8079
    Signed-off-by: Anand Ramachandran <anand.nv1998@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3804,6 +3804,12 @@ public final class Settings {
         public static final String SYSTEMUI_RECENTS_MEM_DISPLAY = "systemui_recents_mem_display";
 
         /**
+         * Whether to show the battery info on the lockscreen while charging
+         * @hide
+         */
+        public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
